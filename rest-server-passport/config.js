33dbc8f043666a31d0428b552a819ed55bd03936
Alexis Hoffman <azguard@iinet.net.au>
2016-08-27 17:33:16 +0800

    Add Facebook OAuth support

--- rest-server-passport/config.js
+++ rest-server-passport/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '1222535637768769',
+        clientSecret: '4c6132ec6ef108e9317402be4748de3d',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
 }
